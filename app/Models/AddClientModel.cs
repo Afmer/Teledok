fc9ad7b91c51392ceae75b85a3681bd3c3b83444
Afmer <afmerlord@gmail.com>
2023-10-06 17:34:11 +0300

    update add client model

--- app/Models/AddClientModel.cs
+++ app/Models/AddClientModel.cs
@@ -1,11 +1,34 @@
 using System.ComponentModel.DataAnnotations;
 using app.Architecture.DataModel;
+using app.Architecture.Enums;
 
 namespace app.Models;
 
 public class AddClientModel
 {
     [Required]
-    public Client Client {get; set;} = null!;
-    public Founder[] Founders {get; set;} = null!;
+    [StringLength(10, MinimumLength = 10, ErrorMessage = "Длина ИНН должна быть 10")]
+    [Display(Name = "ИНН Клиента")]
+    public string ClientINN {get; set;} = null!;
+    [Required]
+    [MaxLength(100)]
+    public string ClientName {get; set;} = null!;
+    [Required]
+    public ClientType ClientType {get; set;}
+    public FounderInfo[] Founders {get; set;} = null!;
+}
+
+public class FounderInfo
+{
+    [Required]
+    [StringLength(10, MinimumLength = 10)]
+    public string INN {get; set;} = null!;
+    [Required]
+    [MaxLength(50)]
+    public string Name {get; set;} = null!;
+    [Required]
+    [MaxLength(50)]
+    public string Surname {get; set;} = null!;
+    [MaxLength(50)]
+    public string Patronymic {get; set;} = null!;
 }
