11c760d37603b49c4eadc3c22aeb82ebf3fa6e63
Afmer <afmerlord@gmail.com>
2023-10-13 01:26:20 +0300

    Create style for forms

--- app/Models/AddClientModel.cs
+++ app/Models/AddClientModel.cs
@@ -6,12 +6,12 @@ namespace app.Models;
 
 public class AddClientModel
 {
-    [Required]
+    [Required(ErrorMessage = "Поле ИНН не может быть пустым")]
     [StringLength(10, MinimumLength = 10, ErrorMessage = "Длина ИНН должна быть 10")]
     [RegularExpression(@"^\d{10}$", ErrorMessage = "Поле должно содержать ровно 10 цифр.")]
     [Display(Name = "ИНН Клиента")]
     public string ClientINN {get; set;} = null!;
-    [Required]
+    [Required(ErrorMessage = "Поле клиента не может быть пустым")]
     [MaxLength(100)]
     public string ClientName {get; set;} = null!;
     [Required]
