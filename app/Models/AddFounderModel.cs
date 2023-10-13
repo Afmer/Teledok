64f18a96c707e38afdf64a8e3fdc4c3262c6ade3
Afmer <afmerlord@gmail.com>
2023-10-13 05:07:14 +0300

    Add right error message

--- app/Models/AddFounderModel.cs
+++ app/Models/AddFounderModel.cs
@@ -4,15 +4,17 @@ namespace app.Models;
 
 public class AddFounderModel
 {
-    [Required]
-    [StringLength(10, MinimumLength = 10)]
+    [Required(ErrorMessage = "Поле ИНН не может быть пустым")]
+    [StringLength(10, MinimumLength = 10, ErrorMessage = "Длина ИНН должна быть 10")]
+    [RegularExpression(@"^\d{10}$", ErrorMessage = "Поле должно содержать ровно 10 цифр.")]
     public string INN {get; set;} = null!;
-    [Required]
+    [Required(ErrorMessage = "Имя обязательно")]
     [MaxLength(50)]
     public string Name {get; set;} = null!;
-    [Required]
+    [Required(ErrorMessage = "Фамилия обязательна")]
     [MaxLength(50)]
     public string Surname {get; set;} = null!;
     [MaxLength(50)]
+    [Required(ErrorMessage = "Отчество обязательно")]
     public string Patronymic {get; set;} = null!;
 }
